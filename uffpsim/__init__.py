2f26f7e17fc623fa118727e9b44bf8fc45d49052
Rajendra Kumar <rjdkmr@gmail.com>
2026-03-17 11:58:35 +0530

    fixed multi-file create db issue

--- uffpsim/__init__.py
+++ uffpsim/__init__.py
@@ -4,6 +4,7 @@ from .fingerprints import FPCalculator, FP_TYPE_DEFAULT_PARAMETERS
 from .fp_supplier import mol_fp_supplier, it_mol_supplier, smi_mol_supplier, sdf_mol_supplier, get_mol_supplier
 from .fp_parallel_supplier import mol_fp_parallel_supplier
 from .database import create_database, create_database_parallel, update_database, redo_inner_clustering, get_database_info
+from .database import create_database_from_files, create_database_from_dir
 from .search_engine import UFFPSimSearchEngine
 import importlib.metadata
 
@@ -11,5 +12,5 @@ os.environ["OMP_CANCELLATION"] = "true"
 
 __all__ = ["uffpsimLib", "mol_fp_supplier", "FPCalculator", "FP_TYPE_DEFAULT_PARAMETERS", "mol_fp_parallel_supplier",
            "create_database", "create_database_parallel", "update_database", "get_database_info",
-            "redo_inner_clustering" ,"UFFPSimSearchEngine"]
+            "redo_inner_clustering" ,"UFFPSimSearchEngine", "create_database_from_files", "create_database_from_dir"]
 __version__ = importlib.metadata.version(__package__ or __name__)
